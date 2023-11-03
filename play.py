9a51bfed8729c308788484edf3d54b8bf7406621
loicland <landrieu.loic@gmail.com>
2023-11-03 17:15:56 +0100

    first commit

---
+++ play.py
@@ -0,0 +1,252 @@
+import os
+import tkinter as tk
+import pandas as pd
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import reverse_geocoder as rg
+import cartopy.crs as ccrs
+import cartopy.geodesic as cgeo
+import cartopy.feature as cfeature
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from math import radians, sin, cos, sqrt, asin, exp
+import argparse
+
+def haversine(lat1, lon1, lat2, lon2):
+    if (lat1 is None) or (lon1 is None) or (lat2 is None) or (lon2 is None):
+        return 0
+    R = 6371  # radius of the earth in km
+    dLat = radians(lat2 - lat1)
+    dLon = radians(lon2 - lon1)
+    a = (
+        sin(dLat / 2.0) ** 2
+        + cos(radians(lat1)) * cos(radians(lat2)) * sin(dLon / 2.0) ** 2
+    )
+    c = 2 * asin(sqrt(a))
+    distance = R * c
+    return distance
+
+def geoscore(d):
+    return 5000 * exp(-d / 1492.7)
+
+class ImageSorter:
+    def __init__(self, args, master, images, coordinates, admins):
+        self.master = master
+        self.images = images
+        self.coordinates = coordinates
+        self.admins = admins
+        self.source_folder = args.image_folder
+        self.index = 0     
+          
+        # Initialize the score and distance lists
+        self.scores = []
+        self.distances = []
+        
+        self.clicked_locations = []
+
+        master.title("Image Viewer")
+        self.frame = tk.Frame(master)
+        self.frame.pack()
+
+        self.canvas = tk.Canvas(self.frame)
+        self.canvas.pack(side="left")
+
+        self.label = tk.Label(self.frame)
+        self.label.pack(side="right")
+        
+        self.result_text_widget = tk.Text(master, height=3, width=100, font=("TkDefaultFont", 10))  # Adjusted for a larger and bigger font text box
+        self.result_text_widget.pack()
+        
+        # Textbox for average score and distance display
+        self.average_text_widget = tk.Text(master, height=4   , width=100, font=("TkDefaultFont", 10))
+        self.average_text_widget.pack()
+        
+        # Configure text color tags
+        self.result_text_widget.tag_configure('yes_tag', foreground='green')
+        self.result_text_widget.tag_configure('no_tag', foreground='red')
+        self.result_text_widget.tag_configure('nan_tag', foreground='grey')
+
+        
+        # Create the figure and canvas only once
+        self.fig = plt.Figure(figsize=(10, 6))
+        self.ax = self.fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.canvas)
+        
+        # Textbox for distance display
+        self.canvas_widget = self.canvas.get_tk_widget()
+        self.canvas_widget.pack()
+
+        # Load initial image
+        self.load_image()
+     
+    def load_image(self):
+        if self.index > len(self.images)-1:
+            self.result_text_widget.delete('1.0', tk.END)
+            self.result_text_widget.insert('end', "Results processing, please wait")
+            
+            self.master.update_idletasks()
+            self.finish()
+            
+            self.master.bind('<space>', lambda event: self.exit_application())
+
+
+        image_path = os.path.join(self.source_folder, f"{self.images[self.index]}.jpg")
+        pil_image = Image.open(image_path)
+        self.tk_image = ImageTk.PhotoImage(pil_image)
+        self.label.config(image=self.tk_image)
+
+        # Clear the canvas and create a new map
+        self.ax.clear()
+        self.ax.set_global()
+        self.ax.stock_img()
+        self.ax.add_feature(cfeature.COASTLINE)
+        self.ax.add_feature(cfeature.BORDERS, linestyle=':')
+        
+        self.result_text_widget.delete('1.0', tk.END)
+        self.result_text_widget.insert('end', "Click the map")
+               
+        self.canvas.draw()
+        
+        self.canvas.mpl_connect('button_press_event', self.on_map_click)
+        
+    def on_map_click(self, event):
+        if event.inaxes:  # Check if click was inside the axes
+            click_lon, click_lat = event.xdata, event.ydata
+
+            # Display true location and line
+            self.display_true_location_and_line(click_lon, click_lat)
+
+            # Unbind the map click event to avoid multiple clicks
+            self.canvas.mpl_disconnect(self.canvas.mpl_connect('button_press_event', self.on_map_click))
+        
+        
+    def display_true_location_and_line(self, click_lon, click_lat):
+    # This new method will display the true location and draw the line
+    
+        self.clicked_locations.append((click_lat,click_lon))
+    
+        true_lon, true_lat = self.coordinates[self.index]
+        self.ax.plot(true_lon, true_lat, 'ro', transform=ccrs.Geodetic())
+        
+        # Draw geodesic line immediately
+        self.ax.plot([true_lon, click_lon], [true_lat, click_lat], 
+                    color='blue', linewidth=2, 
+                    transform=ccrs.Geodetic())
+        
+        self.canvas.draw()
+        
+      
+        distance = haversine(true_lat, true_lon, click_lat, click_lon)
+        score = geoscore(distance)
+        self.scores.append(score)
+        self.distances.append(distance)
+        
+        self.update_average_display()
+         
+        # Set the text for the label or text variable
+        self.result_text_widget.delete('1.0', tk.END)
+        result_text = (f"GeoScore: {score:.0f}, "
+                       f"distance: {distance:.0f} km\n"
+                       "Press space")
+        self.result_text_widget.insert('end', result_text)
+        
+        self.master.bind('<KeyPress>', self.on_key_press)  
+    
+    def on_key_press(self, event):
+        # Go to the next image on any key press
+        self.next_image()
+
+    def next_image(self):
+        # Go to the next image
+        self.index = (self.index + 1)
+        self.load_image()
+        
+        # Re-bind the map click event for the new image
+        self.canvas.mpl_connect('button_press_event', self.on_map_click)
+
+        # Unbind key press to avoid skipping images accidentally
+        self.master.unbind('<KeyPress>')
+        
+        
+    def update_average_display(self):
+        # Calculate the average values
+        avg_score = sum(self.scores) / len(self.scores) if self.scores else 0
+        avg_distance = sum(self.distances) / len(self.distances) if self.distances else 0
+
+        # Update the text box
+        self.average_text_widget.delete('1.0', tk.END)
+        self.average_text_widget.insert('end', f"Average GeoScore: {avg_score:.0f}, "
+                                               f"Average distance: {avg_distance:.0f} km\n")
+    
+    def finish(self):
+        
+        clicks = rg.search(self.clicked_locations)
+        clicked_admins = [[click['name'], click['admin2'], click['admin1'], click['cc']] for click in clicks]
+        
+        correct = [0,0,0,0]
+        valid = [0,0,0,0]
+        
+        for clicked_admin, true_admin in zip(clicked_admins, self.admins):
+            for i in range(4):
+                if true_admin[i]!= 'nan':
+                    valid[i] += 1
+                if true_admin[i] == clicked_admin[i]:
+                    correct[i] += 1
+                    
+        avg_city_accuracy = correct[0] / valid[0]
+        avg_area_accuracy = correct[1] / valid[1]
+        avg_region_accuracy = correct[2] / valid[2]
+        avg_country_accuracy = correct[3] / valid[3]
+        
+        avg_score = sum(self.scores) / len(self.scores) if self.scores else 0
+        avg_distance = sum(self.distances) / len(self.distances) if self.distances else 0
+        
+         # Update the text box
+        self.average_text_widget.delete('1.0', tk.END)
+        exit_message = (f"Average GeoScore: {avg_score:.0f}, "
+                                               f"Average distance: {avg_distance:.0f} km, "
+                                               f"Country Acc: {100*avg_country_accuracy:.1f}, "
+                                               f"Region Acc: {100*avg_region_accuracy:.1f}, "
+                                               f"Area Acc: {100*avg_area_accuracy:.1f}, "
+                                               f"City Acc: {100*avg_city_accuracy:.1f}\n"
+                                               f"Please copy and paste this line to Loic :)\n"
+                                               "Press space to exit")
+        self.average_text_widget.insert('end', exit_message)
+        
+        print(exit_message)
+        
+    def exit_application(self):
+        print("bye")
+        self.master.unbind('<space>')
+        # Destroy the main window and exit the application
+        self.master.destroy()   
+
+def load_images_and_coordinates(csv_file):
+    # Load the CSV
+    df = pd.read_csv(csv_file)
+
+    # Get the image filenames and their coordinates
+    image_ids = df['image_id'].tolist()
+    coordinates = df[['longitude', 'latitude']].values.tolist()
+    admins = df[['city', 'area', 'region', 'country']].values.tolist()
+
+    return image_ids[:3], coordinates[:3], admins[:3]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Image Viewer with Map')
+    parser.add_argument('--image_folder', type=str, required=False, help='Folder with images', default='./select')
+    parser.add_argument('--csv_file', type=str, required=False, help='CSV file with image ids and coordinates', default='/select.csv')
+
+    args = parser.parse_args()
+       
+    images, coordinates, admins = load_images_and_coordinates(args.csv_file)
+
+    root = tk.Tk()
+    root.tk.call('tk', 'scaling', 4.0)  # Adjust the 4.0 to whatever scale factor is needed for your display
+
+    app = ImageSorter(args, root, images, coordinates, admins)
+
+    root.mainloop()
+    
+    
---
+++ selecting.py
@@ -0,0 +1,161 @@
+import os
+import csv
+import argparse
+import pandas as pd
+import tkinter as tk
+from PIL import Image, ImageTk
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import reverse_geocoder as rg
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import shutil
+
+class ImageSorter:
+    def __init__(self, args, master, images, coordinates):
+        self.master = master
+        self.images = images
+        self.coordinates = coordinates
+        self.source_folder = args.source_folder
+        self.select_folder = args.select_folder
+        self.index = 0
+
+        master.title("Image Viewer")
+        self.frame = tk.Frame(master)
+        self.frame.pack()
+
+        self.canvas = tk.Canvas(self.frame)
+        self.canvas.pack(side="left")
+
+        self.label = tk.Label(self.frame)
+        self.label.pack(side="right")
+        
+        # Create the figure and canvas only once
+        self.fig = plt.Figure(figsize=(5, 3))
+        self.ax = self.fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.canvas)
+        self.canvas_widget = self.canvas.get_tk_widget()
+        self.canvas_widget.pack()
+
+        # Load initial image
+        self.load_image()
+
+        # Bind keys
+        master.bind("<Right>", self.next_image)
+        master.bind("<Left>", self.prev_image)
+        master.bind("<space>", self.copy_image)
+
+    def load_image(self):
+        image_path = os.path.join(self.source_folder, f"{self.images[self.index]}.jpg")
+        pil_image = Image.open(image_path)
+        self.tk_image = ImageTk.PhotoImage(pil_image)
+        self.label.config(image=self.tk_image)
+
+        # Clear the canvas and create a new map
+        self.ax.clear()
+        self.ax.set_global()
+        self.ax.stock_img()
+        self.ax.add_feature(cfeature.COASTLINE)
+        self.ax.add_feature(cfeature.BORDERS, linestyle=':')
+
+        # Add the location of the image as a red dot
+        lon, lat = self.coordinates[self.index]
+        self.ax.plot(lon, lat, 'ro', transform=ccrs.Geodetic())
+        
+        self.canvas.draw()
+
+    def next_image(self, event):
+        # Go to the next image
+        self.index = (self.index + 1) % len(self.images)
+        self.load_image()
+
+    def prev_image(self, event):
+        # Go to the previous image
+        self.index = (self.index - 1) % len(self.images)
+        self.load_image()
+
+    def copy_image(self, event):
+        # Copy the current image to the SELECT_FOLDER
+        current_image_path = os.path.join(self.source_folder, f"{self.images[self.index]}.jpg")
+        destination_path = os.path.join(self.select_folder, f"{self.images[self.index]}.jpg")
+        shutil.copy(current_image_path, destination_path)
+
+def load_images_and_coordinates(csv_file, source_folder):
+    # Load the CSV
+    df = pd.read_csv(csv_file)
+    
+    df = df.sample(n=1000)
+
+    # Get the image filenames and their coordinates
+    image_ids = df['image_id'].tolist()
+    coordinates = df[['longitude', 'latitude']].values.tolist()
+    
+    
+
+    # Filter out images that don't exist
+    valid_images = [img_id for img_id in image_ids if os.path.exists(os.path.join(source_folder, f"{img_id}.jpg"))]
+    valid_coordinates = [coordinates[image_ids.index(img_id)] for img_id in valid_images]
+
+    return valid_images, valid_coordinates
+
+
+def create_select_csv(select_folder, original_csv, output_csv_path):
+    # Read the original CSV and create a mapping from image_id to coordinates
+    image_to_coords = {}
+    with open(original_csv, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            image_to_coords[row['image_id']] = (float(row['latitude']), float(row['longitude']))  # reverse_geocoder expects (lat, lon)
+
+    # List the files in the select_folder
+    selected_files = [f for f in os.listdir(select_folder) if os.path.isfile(os.path.join(select_folder, f))]
+
+    # Write the select.csv file
+    with open(output_csv_path, 'w', newline='') as csvfile:
+        fieldnames = ['image_id', 'longitude', 'latitude', 'city', 'area', 'region', 'country']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        
+        for file in selected_files:
+            # Extract image_id from filename
+            image_id = os.path.splitext(file)[0]
+            if image_id in image_to_coords:
+                # Perform reverse geocoding
+                coordinates = image_to_coords[image_id]
+                results = rg.search(coordinates)  # Returns a list of dictionaries
+                result = results[0]  # Get the first result
+
+                # Write to CSV if image_id exists in the mapping
+                writer.writerow({
+                    'image_id': image_id,
+                    'longitude': coordinates[1],
+                    'latitude': coordinates[0],
+                    'city': result['name'],
+                    'area': result['admin2'],
+                    'region': result['admin1'],
+                    'country': result['cc']
+                })
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Image Viewer with Map')
+    parser.add_argument('--source_folder', type=str, required=False, help='Folder with images', default='/home/ign.fr/llandrieu/Documents/code/geoscrapping/images/test')
+    parser.add_argument('--select_folder', type=str, required=False, help='Folder to copy selected images into', default='/home/ign.fr/llandrieu/Documents/code/geoscrapping/images/select')
+    parser.add_argument('--csv_file', type=str, required=False, help='CSV file with image ids and coordinates', default='/home/ign.fr/llandrieu/Documents/code/geoscrapping/processed/test.csv')
+
+    args = parser.parse_args()
+
+    # Ensure the select folder exists
+    if not os.path.exists(args.select_folder):
+        os.makedirs(args.select_folder)
+        
+    if False:
+
+        images, coordinates = load_images_and_coordinates(args.csv_file, args.source_folder)
+
+        root = tk.Tk()
+        app = ImageSorter(args, root, images, coordinates)
+        root.mainloop()
+    
+    else:
+        create_select_csv(args.select_folder, args.csv_file, '/home/ign.fr/llandrieu/Documents/code/geoscrapping/processed/select.csv')
+    
